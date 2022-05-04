6476c19e7c30f795aa68f697b39917f44a00df0d
kmitch92 <kielmitchell8@gmail.com>
2022-05-04 09:25:17 +0100

    first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+    let result = ''
+    if (times < 0) return 'ERROR'
+    else {
+        for (let i = 0; i < times; i++) {
+            result = result + string
+        }
+        return result
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let splitArr = string.split('');
+    let result = ''
+
+    for (let i = splitArr.length - 1; i >= 0; i--) {
+        result = result + splitArr[i]
+    }
+
+    return result
 
 };
 
